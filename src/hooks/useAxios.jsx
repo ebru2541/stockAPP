03cf82d539879017279b500fb90b13c03070fd62
Rose <ebrue7949@gmail.com>
2023-07-09 20:17:02 +0300

    first commit

---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+const useAxios = () => {
+  const { token } = useSelector((state) => state.auth);
+  console.log(token);
+  const axiosPublic = axios.create({
+    baseURL: "https://13800.fullstack.clarusway.com/",
+  });
+
+  const axiosWithToken = axios.create({
+    baseURL: "https://13800.fullstack.clarusway.com/",
+    headers: { Authorization: `Token ${token}` },
+  });
+
+  return { axiosWithToken, axiosPublic };
+};
+
+export default useAxios;
